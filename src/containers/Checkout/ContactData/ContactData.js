e1cbc3d4975d87a279fb129ce7cdc0f74db5b9a3
Gerard Eklu <gerard_eklu@homedepot.com>
2020-01-08 19:20:16 -0500

    Adjusting checkout and contact data

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -4,7 +4,8 @@ import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import classes from './ContactData.css';
 import axios from '../../../axios-orders';
-import Input from "../../../components/UI/Input/Input"
+import Input from "../../../components/UI/Input/Input";
+import { connect } from "react-redux"
 
 class ContactData extends Component {
     state = {
@@ -74,7 +75,7 @@ class ContactData extends Component {
         }
         console.log(formData)
         const order = {
-            ingredients: this.props.ingredients,
+            ingredients: this.props.ings,
             price: this.props.price,
             orderData: formData
         }
@@ -135,4 +136,12 @@ class ContactData extends Component {
     }
 }
 
-export default ContactData;
+
+const mapStateToProps = state => {
+    return {
+        ings: state.ingredients,
+        price: state.totalPrice
+    }
+}
+
+export default connect(mapStateToProps)(ContactData);
